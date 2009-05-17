40611273a083b7f5234d38c845d4f40ad7c3cf14
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2009-05-17 13:51:32 +0200

    added kvar_test dir

---
+++ test/kvar_test/test.c
@@ -0,0 +1,33 @@
+#include "kvar.h"
+#include <stdio.h>
+
+int main(void)
+{
+    kvar_storage_ptr store = kvar_create_storage();
+    kvar_print_vars(store);    
+    kvar_var_ptr num = kvar_create_int(17);
+    kvar_set(store, "bhallo", num);
+    num = kvar_create_int(11);
+    kvar_set(store, "foo", num);
+
+    int foo = kvar_extract_int(kvar_get(store, "foo"));
+    printf ("foo is: %d\n", foo);
+
+    kvar_print_vars(store);    
+
+    int nums[10] = {13,6,3,4,5,6,7,8,9,0};
+    kvar_var_ptr arr = kvar_create_array(nums, 10);
+    kvar_set(store, "arr", arr);
+    
+    foo = kvar_extract_int(kvar_get(store, "arr"));
+    printf ("foo is: %d\n", foo);
+
+    num = kvar_create_int(30);
+    kvar_set(store, "foo", num);
+
+    kvar_delete(store, "hallo");
+    kvar_print_vars(store);    
+    kvar_dispose_storage(store);    
+
+    return 0;
+}
