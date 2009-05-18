813f1b2e5f8d8cf60d71c4bd64d2cd4a26252be7
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2009-05-18 17:03:18 +0200

    modified some files to use syntax.c

--- kalt/src/kalt.c
+++ kalt/src/kalt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "parser.h"
+#include "syntax.h"
 
 #define VERSION 1
 
@@ -7,8 +8,22 @@
 int main(void)
 {
     printf("Welcome to kalt - Krettek Algorithmus Tester version %d\n", VERSION);
-    parse_result result = parse_tokenize("= test1 = [1a   ,2,3]");
+    parse_result result = parse_tokenize("hallo = aljoshca(ali,foom = ali)");
+    //parse_result result = parse_tokenize("foom = ");
     parse_dump(result);
-    parse_dispose_result(result);
+    syntax_result syn_result = syntax_check(result);
+    if (syn_result.success == 1)
+    {
+        printf("Syntax correct.\n");
+    }
+    else
+    {
+        printf("Syntax error: %s\n", syn_result.error_str);
+    }
+    /*parse_result subset = parse_create_subset(result,8,9);
+    parse_dump(subset);
+    subset = parse_create_subset(subset, 0,0);
+    parse_dump(subset);
+    parse_dispose_result(result);*/
     return 0;
 }
