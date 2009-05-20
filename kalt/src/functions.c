d9d8520e9ca6229240c2d6b58682d3cce86d6324
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2009-05-20 14:30:17 +0200

    added bubblesort and some modifications

--- kalt/src/functions.c
+++ kalt/src/functions.c
@@ -147,26 +147,50 @@ kvar_var_ptr dyson_descending(int param_count, kvar_storage_ptr store)
     return result;
 }
 
-
 long int quicksort_p, quicksort_s, quicksort_c;
+int bubblesort(int arr[], int first, int last)
+{
+    int index, temp;
+    int running = 1;
+    while (running && last >= 0) // the second comparison is not counted
+    {
+        quicksort_c++; // we only call bubblesort from quicksort
+        running = 0;
+        for (index = first; index < last; index++)
+        {
+            quicksort_c++;
+            if (arr[index] > arr[index+1])
+            {
+                running = 1;
+                temp = arr[index];
+                arr[index] = arr[index+1];
+                arr[index+1] = temp;
+                quicksort_s++;
+            }
+        }
+        last--;
+    }
+}
+
 #define FIRST 1
 #define MEDIAN 2
 #define RANDOM_MEDIAN 3
+#define BUBBLE_LENGTH 0 // The partition length at which to resort to bubblesort
+int bubble_length = BUBBLE_LENGTH;
 int quicksort_pivot_selection_method = 1;
 int quicksort(int arr[], int first, int last)
 {
     int i, pivot, new_val;
     int m1, m2, m3;
+    quicksort_c++;
+    if (last >= first && (((last-first)+1) <= bubble_length))
+    {
+        quicksort_p++;
+        return bubblesort(arr,first,last);
+    }
     if (last > first)
     {
-        /*for(i = first; i <= last; i++)
-        {
-            printf("%d ",arr[i]);
-        }
-        printf("\n");*/
-
-        quicksort_c++;
-        //Pivot selection
+        quicksort_p++;
         switch(quicksort_pivot_selection_method)
         {
             case FIRST:
@@ -176,20 +200,12 @@ int quicksort(int arr[], int first, int last)
                 m1 = first;
                 m2 = (first+last) / 2;
                 m3 = last;
-                /*printf("m1[%d:%d]  m2[%d:%d]  m2[%d:%d]\n", m1, arr[m1], m2, arr[m2], m3, arr[m3]);*/
                 if ((arr[m1] <= arr[m3] && arr[m3] <= arr[m2]) || (arr[m2] <= arr[m3] && arr[m3] <= arr[m1]))
                     pivot = m3;
                 else if ((arr[m2] <= arr[m1] && arr[m1] <= arr[m3]) || (arr[m3] <= arr[m1] && arr[m1] <= arr[m2]))
                     pivot = m1;
                 else
                     pivot = m2; 
-                /*if(pivot == m2)
-                {
-                }//printf("selected middle elem\n");
-                else
-                {
-                    printf("not selected middle elem\n");
-                }*/
                 new_val = arr[first];
                 arr[first] = arr[pivot];
                 arr[pivot] = new_val;
@@ -215,9 +231,9 @@ int quicksort(int arr[], int first, int last)
         for (i = first+1; i <= last; i++, quicksort_c++)
         {
             new_val = arr[i];
+            quicksort_c++;
             if (new_val < arr[pivot])
             {   
-                quicksort_c++;
                 arr[i] = arr[pivot+1];
                 arr[pivot+1] = arr[pivot];
                 arr[pivot] = new_val;
@@ -225,19 +241,14 @@ int quicksort(int arr[], int first, int last)
                 pivot++;
             }
         }
-        /*for(i = first; i < pivot; i++)
-        {
+        /*for ( i = first; i < pivot; i++)
             printf("%d ",arr[i]);
-        }
         printf("(%d) ", arr[pivot]);
-        for(i = pivot+1; i <= last; i++)
-        {
+        for ( i = pivot+1; i <= last; i++)
             printf("%d ",arr[i]);
-        }
         printf("\n");*/
         quicksort(arr, first, pivot-1);
         quicksort(arr, pivot+1, last);
-        quicksort_p += 2;
     }
     return 1;
 }
@@ -259,10 +270,10 @@ kvar_var_ptr quicksort_interface(int param_count, kvar_storage_ptr store)
         printf("param 1 must be an array\n");
         return NULL;
     }
+    bubble_length = 0;
     count = kvar_extract_array_length(param1);
     arr = kvar_extract_array(param1);
     quicksort_p = quicksort_s = quicksort_c = 0;
-    quicksort_p = 1;
     quicksort_pivot_selection_method = FIRST; 
     quicksort(arr,0, count-1);
     printf("quicksort variant 1 (select first element as pivot): \n");
@@ -285,6 +296,7 @@ kvar_var_ptr quicksort_test_interface(int param_count, kvar_storage_ptr store)
         printf("quicksort: usage: quicksort(array)\n");
         return NULL;
     }
+    int selected_bubble_length = 7;
 
     kvar_copy(store,"_temp1", "_temp");
 
@@ -295,10 +307,11 @@ kvar_var_ptr quicksort_test_interface(int param_count, kvar_storage_ptr store)
         return NULL;
     }
 
+    // without bubble sort
+    bubble_length = 0;
     count = kvar_extract_array_length(param1);
     arr = kvar_extract_array(param1);
     quicksort_p = quicksort_s = quicksort_c = 0;
-    quicksort_p = 1;
     quicksort_pivot_selection_method = FIRST; 
     time_diff = clock();
     quicksort(arr,0, count-1);
@@ -318,7 +331,6 @@ kvar_var_ptr quicksort_test_interface(int param_count, kvar_storage_ptr store)
     count = kvar_extract_array_length(param1);
     arr = kvar_extract_array(param1);
     quicksort_p = quicksort_s = quicksort_c = 0;
-    quicksort_p = 1;
     quicksort_pivot_selection_method = MEDIAN; 
     time_diff = clock();
     quicksort(arr,0, count-1);
@@ -338,7 +350,6 @@ kvar_var_ptr quicksort_test_interface(int param_count, kvar_storage_ptr store)
     count = kvar_extract_array_length(param1);
     arr = kvar_extract_array(param1);
     quicksort_p = quicksort_s = quicksort_c = 0;
-    quicksort_p = 1;
     quicksort_pivot_selection_method = RANDOM_MEDIAN; 
     time_diff = clock();
     quicksort(arr,0, count-1);
@@ -348,6 +359,67 @@ kvar_var_ptr quicksort_test_interface(int param_count, kvar_storage_ptr store)
     printf("  c: %ld\n", quicksort_c);
     printf("  runtime: %ld msecs\n", (clock()-time_diff)/1000);
 
+
+    printf("----------------------\n");
+    //with bubblesort
+    bubble_length = selected_bubble_length;
+    kvar_copy(store, "_temp", "_temp1");
+    param1 = kvar_get(store, "_temp1");
+    if (param1->type != kvar_type_array)
+    {
+        printf("param 1 must be an array\n");
+        return NULL;
+    }
+    count = kvar_extract_array_length(param1);
+    arr = kvar_extract_array(param1);
+    quicksort_p = quicksort_s = quicksort_c = 0;
+    quicksort_pivot_selection_method = FIRST; 
+    time_diff = clock();
+    quicksort(arr,0, count-1);
+    printf("quicksort variant 1 (select first element as pivot)(change to bubblesort at partitionsize %d): \n", bubble_length);
+    printf("  p: %ld\n", quicksort_p);
+    printf("  s: %ld\n", quicksort_s);
+    printf("  c: %ld\n", quicksort_c);
+    printf("  runtime: %ld msecs\n", (clock()-time_diff)/1000);
+
+    kvar_copy(store, "_temp", "_temp1");
+    param1 = kvar_get(store, "_temp1");
+    if (param1->type != kvar_type_array)
+    {
+        printf("param 1 must be an array\n");
+        return NULL;
+    }
+    count = kvar_extract_array_length(param1);
+    arr = kvar_extract_array(param1);
+    quicksort_p = quicksort_s = quicksort_c = 0;
+    quicksort_pivot_selection_method = MEDIAN; 
+    time_diff = clock();
+    quicksort(arr,0, count-1);
+    printf("quicksort variant 2 (select median of start median and end as pivot)(change to bubblesort at partitionsize %d): \n", bubble_length);
+
+    printf("  p: %ld\n", quicksort_p);
+    printf("  s: %ld\n", quicksort_s);
+    printf("  c: %ld\n", quicksort_c);
+    printf("  runtime: %ld msecs\n", (clock()-time_diff)/1000);
+
+    kvar_copy(store, "_temp", "_temp1");
+    param1 = kvar_get(store, "_temp1");
+    if (param1->type != kvar_type_array)
+    {
+        printf("param 1 must be an array\n");
+        return NULL;
+    }
+    count = kvar_extract_array_length(param1);
+    arr = kvar_extract_array(param1);
+    quicksort_p = quicksort_s = quicksort_c = 0;
+    quicksort_pivot_selection_method = RANDOM_MEDIAN; 
+    time_diff = clock();
+    quicksort(arr,0, count-1);
+    printf("quicksort variant 3 (select median of 3 randoms as pivot)(change to bubblesort at partitionsize %d): \n", bubble_length);
+    printf("  p: %ld\n", quicksort_p);
+    printf("  s: %ld\n", quicksort_s);
+    printf("  c: %ld\n", quicksort_c);
+    printf("  runtime: %ld msecs\n", (clock()-time_diff)/1000);
     result = kvar_create_array(arr, count);
     return result;
 }
